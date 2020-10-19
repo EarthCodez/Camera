074ec9748a445c694a507f31e75d453b05121573
RealParamesh <71922456+RealParamesh@users.noreply.github.com>
2020-10-19 15:51:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let cam;
 function setup() {
+  createCanvas(200,200);
   cam=createCapture(VIDEO);
 }
 
 function draw() {
   background(220);
   fill(random(0,255),random(0,255),random(0,255));
+  cam.position(0,0);
           }
