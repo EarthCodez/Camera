08d72170c8d167212ca6eb4be19bc5da384f12b2
Paramesh S <71922456+RealParamesh@users.noreply.github.com>
2021-07-04 16:31:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,84 +1,86 @@
-var sslide;
-var rslide;
-var gslide;
-var bslide;
-var cbutton;
-var dbutton;
-var state="nod";
-var ms;
-var ssr;
-
+let cam;
+let app;
+var camof;
+var tintn;
+var ftr;
+var svbtn;
+var colorpic;
+var hs1,hs2,hs3;
+var cosh=true;
 function setup() {
-  createCanvas(windowWidth-10,windowHeight-65);
-  sslide= createSlider(0,200,0);
-  rslide= createSlider(0,255,0);
-  gslide= createSlider(0,255,0);
-  bslide= createSlider(0,255,0);
-  cbutton= createButton("clear");
-  dbutton= createButton("download your pic");
-  ms=createInput("circle");
-  background("black");
- //createP("(size rgb ) and r g b won't be captured in pic it will appear the fill selected & my brand name(not selling anything) , for comments message me at parameshgaming30@gmail.com")
+  createCanvas(640,480);
+  cam=createCapture(VIDEO);
+  camof=0;
+  colorpic=createButton("color of tint");
+  tintn=createSlider(0,255);
+  ftr=createSlider(0,800);
+  svbtn=createButton("save");
+  hs1=createSlider(0,255)
+  hs2=createSlider(0,255)
+  hs3=createSlider(0,255)
+  hs1.position(10,510);
+  hs2.position(150,510);
+  hs3.position(290,510);
+  hs1.hide();
+    hs2.hide();
+      hs3.hide();
+  // app=createInput("255,255,255");
 }
 
 function draw() {
-  textSize(20);
-  noStroke();
-  fill("white");
-  if(mouseIsPressed){
-    fill(rslide.value(),gslide.value(),bslide.value());
-    if(ms.value()=="circle"){
-   ellipse(mouseX,mouseY,sslide.value(),sslide.value()); 
-    }
-    else if(ms.value()=="square"){
-      rect(mouseX,mouseY,sslide.value(),sslide.value());
-    }
-    else if(ms.value()=="triangle"){
-      triangle(mouseX-100, mouseY, mouseX+100, mouseY, mouseX, mouseY-150)
-    }
-    rect(0,0,100,100);
-  } 
-  if(mouseIsPressed){
-    state="d";
+  background(220);
+  cam.position(0,0);
+  if(camof<25){
+  cam.hide();
   }
+  else{
+    cam.show();
+  }
+  image(cam, 0, 0, 640, 480);
+  if(ftr.value()<100&&ftr.value()>0){
+    filter(INVERT);
+  }
+  else if(ftr.value()<200&&ftr.value()>100){
+    filter(THRESHOLD);
+  }
+    else if(ftr.value()<300&&ftr.value()>200){
+    filter(GRAY);
+  }
+    else if(ftr.value()<400&&ftr.value()>300){
+    filter(OPAQUE);
+  }
+    else if(ftr.value()<500&&ftr.value()>400){
+    filter(POSTERIZE);
+  }
+    else if(ftr.value()<600&&ftr.value()>500){
+    filter(DILATE);
+  }
+    else if(ftr.value()<700&&ftr.value()>600){
+    filter(BLUR);
+  }
+    else if(ftr.value()<800&&ftr.value()>700){
+    filter(ERODE);
+    }
+    // console.log(colorpic.color());
+  fill(hs1.value(),hs2.value(),hs3.value(),tintn.value());
   
-  if(state=="nod"){
-     text(rslide.value(),30,30);  
-     text(gslide.value(),30,50); 
-     text(bslide.value(),30,70); 
-     text("r",10,30);  
-     text("g",10,45); 
-     text("b",10,70); 
-     text("size,rgb",20,height-5);
-}
-ssr=sslide.value()
-  cbutton.mousePressed(cllear);
-  dbutton.mousePressed(download);
-}
-function cllear(){
-  fill('black');
-  sslide.value(0);
-  rslide.value(0);
-  gslide.value(0);
-  bslide.value(0);
- ellipse(200,200,10000,10000);
-//  console.log(sslide.value());
-//  mouseReleased(sslide.value(0));
-}
-function download(){
-  state="d";
-  if(state=="d"){
-  fill(rslide.value(),gslide.value(),bslide.value());
-  rect(0,0,100,100);
-  rect(20,height-20,70,20);
-    fill("white");
-    textSize(10);
-     text("parameshCodes",5,10);
-  save();
-  }
-}
+  rect(0,0,width,height)
+  svbtn.mousePressed(save);
+  colorpic.mousePressed(tintopt);
+  if(frameCount%2000==0){
+    hs1.hide();
+    hs2.hide();
+    hs3.hide();
+      cosh=true;
+    }
+          }
 
+          function tintopt(){
 
-function mouseReleased(){
-    (state="nod");
-}
+            if(cosh==true){
+            hs1.show();
+            hs2.show();
+            hs3.show();
+            cosh=false;
+            }
+          }
